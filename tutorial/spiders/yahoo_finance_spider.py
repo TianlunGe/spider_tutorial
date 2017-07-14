7d026c4c0081a80d01e71a00a69489bb0b6d1bf2
TianlunGe <ge1tianlun@163.com>
2017-07-14 17:57:07 +0800

    spider

--- tutorial/spiders/yahoo_finance_spider.py
+++ tutorial/spiders/yahoo_finance_spider.py
@@ -4,9 +4,12 @@ import scrapy
 class finance_spider(scrapy.spiders.Spider):
     name = 'yahoo'
     allowed_domains = ['yahoo.com']
-    start_urls = ['https://finance.yahoo.com/quote/AAPL/financials?p=AAPL']
+    start_urls = ['https://finance.yahoo.com/quote/AAPL/financials?p=AAPL',
+                  'https://finance.yahoo.com/quote/AAPL/options?ltr=1']
 
     def parse(self, response):
         filename = response.url.split('/')[-2]
+        sel = response.selector
+
         with open(filename, 'wb') as f:
             f.write(response.body)
